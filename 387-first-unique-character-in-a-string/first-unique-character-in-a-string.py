f6077cadc7b536a58221a45a37477c31f6fa9550
Fakeidforservlet <157262167+Fakeidforservlet@users.noreply.github.com>
2024-01-21 09:40:16 +0530

    Time: 73 ms (92.28%) | Memory: 16.9 MB (57.14%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
@@ -1,4 +1,6 @@
 class Solution:
+    #using dic:
+    ''' 
     def firstUniqChar(self, s: str) -> int:
         frequency = {}
         for i in s:
@@ -8,6 +10,18 @@ class Solution:
                 frequency[i]+=1
         for i in s:
             if frequency[i]==1:
+                return s.index(i)
+        return -1
+    '''
+
+    #using array
+    def firstUniqChar(self, s: str) -> int:
+        char_count=[0]*256
+
+        for i in s:
+            char_count[ord(i)]+=1
+        for i in s:
+            if char_count[ord(i)]==1:
                 return s.index(i)
         return -1
         

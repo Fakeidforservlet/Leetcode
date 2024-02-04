185e8fe40014ef2be07e5d595ebd30d43377ad6b
Fakeidforservlet <157262167+Fakeidforservlet@users.noreply.github.com>
2024-02-04 10:18:27 +0530

    Time: 86 ms (95.15%) | Memory: 12.8 MB (94.65%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -4,8 +4,11 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        for i in range(1,len(nums)+1):
-            if 0 not in nums:
-                return 0
-            elif i not in nums:
-                return i
+        # for i in range(1,len(nums)+1):
+        #     if 0 not in nums:
+        #         return 0
+        #     elif i not in nums:
+        #         return i
+        arr_len=len(nums)
+        total_sum=arr_len*(arr_len+1)//2
+        return total_sum-sum(nums)

03dea91e8f1db78da576a0b2778451828dbd3902
Fakeidforservlet <157262167+Fakeidforservlet@users.noreply.github.com>
2024-02-04 13:41:57 +0530

    Time: 18 ms (64.78%) | Memory: 11.5 MB (93.96%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -16,11 +16,13 @@ class Solution(object):
 
         low=1
         high=x
+        result = 0
         while low<=high:
             mid=low+(high-low)//2
             if (mid*mid)<=x:
+                result = mid
                 low=mid+1
             else:
                 high=mid-1
-        return high
+        return result
         

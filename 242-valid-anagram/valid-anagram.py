d74451fcce6002d82133dac7b7303e2df76b47c1
Fakeidforservlet <157262167+Fakeidforservlet@users.noreply.github.com>
2024-02-06 10:17:09 +0530

    Time: 24 ms (92.51%) | Memory: 11.9 MB (98.42%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,8 +1,21 @@
 class Solution(object):
+    def count_character(self,string):
+            hashmap={}
+            for i in string:
+                if i not in hashmap:
+                    hashmap[i]=0
+                else:
+                    hashmap[i]+=1
+            return hashmap
     def isAnagram(self, s, t):
         """
         :type s: str
         :type t: str
         :rtype: bool
         """
-        return ''.join(sorted(s))==''.join(sorted(t))
+        s_hashmap=self.count_character(s)
+        t_hashmap=self.count_character(t)
+
+        return s_hashmap==t_hashmap
+
+        

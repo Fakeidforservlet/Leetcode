025058e219f9b85bd9d6faa03de5a8782663f23b
Fakeidforservlet <157262167+Fakeidforservlet@users.noreply.github.com>
2024-02-11 10:54:10 +0530

    Time: 46 ms (69.40%) | Memory: 12 MB (93.28%) - LeetSync

--- 1184-car-pooling/car-pooling.py
+++ 1184-car-pooling/car-pooling.py
@@ -6,21 +6,21 @@ class Solution(object):
         :rtype: bool
         """
         #https://www.youtube.com/watch?v=nO95uYKB-lo
-        lastDropPoint=0
-        for route in trips:
-            if route[2]>lastDropPoint:
-                lastDropPoint=route[2]
-        Person_count_arr=[0]*(lastDropPoint+1)
+        # lastDropPoint=0
+        # for route in trips:
+        #     if route[2]>lastDropPoint:
+        #         lastDropPoint=route[2]
+        # Person_count_arr=[0]*(lastDropPoint+1)
 
-        for pick_drop in trips:
-            Person_count_arr[pick_drop[1]]=Person_count_arr[pick_drop[1]]+pick_drop[0]
-            Person_count_arr[pick_drop[2]]=Person_count_arr[pick_drop[2]]-pick_drop[0]
-        current_capacity=0
-        for setted_passanger in Person_count_arr:
-            current_capacity+=setted_passanger
-            if current_capacity>capacity:
-                return False
-        return True
+        # for pick_drop in trips:
+        #     Person_count_arr[pick_drop[1]]=Person_count_arr[pick_drop[1]]+pick_drop[0]
+        #     Person_count_arr[pick_drop[2]]=Person_count_arr[pick_drop[2]]-pick_drop[0]
+        # current_capacity=0
+        # for setted_passanger in Person_count_arr:
+        #     current_capacity+=setted_passanger
+        #     if current_capacity>capacity:
+        #         return False
+        # return True
 
         #or 
 

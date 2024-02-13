488de1c67a8e519b28653aab4a150ac8e5bcf3eb
Fakeidforservlet <157262167+Fakeidforservlet@users.noreply.github.com>
2024-02-13 12:20:27 +0530

    Time: 343 ms (82.51%) | Memory: 11.7 MB (88.31%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        #https://www.youtube.com/watch?v=vTlVtLvPQo4
+        def expandAroundOrbit(left,right):
+            while left>=0 and right<len(s) and s[left]==s[right]:
+                left-=1
+                right+=1
+            return s[left+1:right]
+        longestPalindromeString=''
+        for i in range(len(s)):
+            oddPalindromeString=expandAroundOrbit(i,i)
+            if len(oddPalindromeString)>len(longestPalindromeString):
+                longestPalindromeString=oddPalindromeString
+            evenPalindromeString=expandAroundOrbit(i,i+1)
+            if len(evenPalindromeString)>len(longestPalindromeString):
+                longestPalindromeString=evenPalindromeString
+        return longestPalindromeString

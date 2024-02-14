de1c67a0e07bd1eda8ebf723da75bd44bde48d0b
Fakeidforservlet <157262167+Fakeidforservlet@users.noreply.github.com>
2024-02-14 19:12:25 +0530

    Time: 11 ms (78.46%) | Memory: 11.8 MB (61.34%) - LeetSync

--- 394-decode-string/decode-string.py
+++ 394-decode-string/decode-string.py
@@ -5,19 +5,36 @@ class Solution(object):
         :rtype: str
         """
         #https://www.youtube.com/watch?v=6yOWLXlWOJc
-        currString=''
-        currNum=0
-        stack = []
+        # currString=''
+        # currNum=0
+        # stack = []
+        # for i in s:
+        #     if i.isdigit():
+        #         currNum=currNum*10+int(i)
+        #     elif i=='[':
+        #         stack.append((currNum,currString))
+        #         currString=''
+        #         currNum=0
+        #     elif i==']':
+        #         prevNum,prevString=stack.pop()
+        #         currString=prevString+ currString*prevNum
+        #     else:
+        #         currString+=i
+        # return currString
+
+        #simple_one
+        stack=[]
         for i in s:
-            if i.isdigit():
-                currNum=currNum*10+int(i)
-            elif i=='[':
-                stack.append((currNum,currString))
-                currString=''
-                currNum=0
-            elif i==']':
-                prevNum,prevString=stack.pop()
-                currString=prevString+ currString*prevNum
+            if i!=']':
+                stack.append(i)
             else:
-                currString+=i
-        return currString
+                subst=''
+                while stack[-1]!='[':
+                    subst=stack.pop()+subst
+                stack.pop()
+                digit=''
+                while stack and stack[-1].isdigit():
+                    digit=stack.pop()+digit
+                stack.append(subst*int(digit))
+        print(''.join(stack))
+        return ''.join(stack)
